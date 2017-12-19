f1b100266ebc89a34854db79f1431f65700f6926
fuvzn121 <c011426930@edu.teu.ac.jp>
2017-12-20 05:03:59 +0900

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: bundle exec ruby foodsearch.rb
--- food_search.rb
+++ food_search.rb
@@ -21,22 +21,24 @@ stream = Twitter::Streaming::Client.new do |config|
   config.access_token_secret = TOKEN_SECRET
 end
 
+my_name = rest.user.screen_name
 stream.user do |object|
-  regex = /食べたい$/
-  if object.is_a?(Twitter::Tweet)
-    if object.user.screen_name == "hrys__" and object.text.match(regex)
-      food = object.text.gsub(regex, "")
-      url = "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=3387d22587c7f29f84cf490110541a26&format=json&areacode_s=AREAS2288&freeword=#{food}&hit_per_page=1"
-      hash = JSON.parse(open(URI.escape url).read)
-      hash =  JSON.parse(open(URI.escape url).read)
-      if hash['rest'].empty?
-        rest.update("@hrys__ \"#{food}\"に関するお店が見つかりません")
-      else
-        name =  hash['rest']['name']
-        link = hash['rest']['url']
-        rest.update("@hrys__ このお店がおすすめです\n#{name}\n#{link}")
-      end
+  regex = /食べたい$|飲みたい$/
+  if (object.is_a?(Twitter::Tweet) && object.text.match(regex)) \
+    && (object.user.screen_name == my_name || object.in_reply_to_screen_name == my_name)
+    id = object.id
+    to_name = object.user.screen_name
+    food = object.text.gsub(regex, "")
+    food = food.gsub(/@.+\s/, "") if object.in_reply_to_screen_name == my_name
+    url = "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=3387d22587c7f29f84cf490110541a26&format=json&areacode_s=AREAS2288&freeword=#{food}"
+    hash =  JSON.parse(open(URI.escape url).read)
+    hash_num = rand(hash["hit_per_page"].to_i)
+    if hash['error']
+      rest.update("@#{to_name} \"#{food}\"に関するお店が見つかりません", in_reply_to_status_id: id)
+    else
+      name =  hash['rest'][hash_num]['name']
+      link = hash['rest'][hash_num]['url']
+      rest.update("@#{to_name} このお店がおすすめです\n#{name}\n#{link}", in_reply_to_status_id: id)
     end
   end
-
 end
